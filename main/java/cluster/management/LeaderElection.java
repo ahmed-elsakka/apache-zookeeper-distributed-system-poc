b9809923b01ecbc3bcfc020aead8d08e4b6500ae
ahmed-elsakka <ahmed.elsakka@outlook.com>
2022-04-01 01:45:34 +0200

    Implement ServiceRegistry Class and OnElectionCallback Interface to add the cluster registeration functionality

--- main/java/cluster/management/LeaderElection.java
+++ main/java/cluster/management/LeaderElection.java
@@ -3,7 +3,6 @@ package cluster.management;
 import org.apache.zookeeper.*;
 import org.apache.zookeeper.data.Stat;
 
-import java.io.IOException;
 import java.util.Collections;
 import java.util.List;
 
@@ -11,9 +10,11 @@ public class LeaderElection implements Watcher {
     private static final String ELECTION_NAMESPACE = "/election";
     private ZooKeeper zooKeeper;
     private String currentZnodeName;
+    private final OnElectionCallback onElectionCallback;
 
-    public LeaderElection(ZooKeeper zooKeeper){
+    public LeaderElection(ZooKeeper zooKeeper, OnElectionCallback onElectionCallback){
         this.zooKeeper = zooKeeper;
+        this.onElectionCallback = onElectionCallback;
     }
 
 
@@ -29,13 +30,14 @@ public class LeaderElection implements Watcher {
     public void reElectLeader() throws InterruptedException, KeeperException {
         Stat predecessorStat = null;
         String predecessorZNodeName = "";
-        
+
         while(predecessorStat == null){
             List<String> children = zooKeeper.getChildren(ELECTION_NAMESPACE, false);
             Collections.sort(children);
             String smallestChild = children.get(0);
             if(smallestChild.equals(currentZnodeName)){
                 System.out.println("Current Node is the Leader node!");
+                onElectionCallback.onElectedToBeLeader();
                 return;
             } else {
                 int predecessorIndex = Collections.binarySearch(children, currentZnodeName) - 1;
@@ -43,6 +45,7 @@ public class LeaderElection implements Watcher {
                 predecessorStat = zooKeeper.exists(ELECTION_NAMESPACE + "/" + predecessorZNodeName, this);
             }
         }
+        onElectionCallback.onWorker();
         System.out.println("Watching node " + predecessorZNodeName);
     }
 
